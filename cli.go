adc5d775c739c96c4a56bb8332c4a32aaa6c854b
Konstantin Zhigaylo <zero@kostyazero.com>
2025-12-22 11:10:25 +0300

    feat(cli): load configuration from config.json file

--- cli.go
+++ cli.go
@@ -1,24 +1,27 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/spf13/cobra"
 )
 
 func BuildCli() *cobra.Command {
+	var config string
 	rootCmd := &cobra.Command{
 		Use:   "dock",
 		Short: "A port for your files.",
 		Run: func(cmd *cobra.Command, args []string) {
-			m := make(map[string]string)
-			m["kostya"] = "123"
-			cfg := Config{
-				Address: ":21",
-				Root:    "D:\\Anime",
-				Users:   m,
+			cfg, err := LoadConfig(config)
+			if err != nil {
+				fmt.Printf("Failed to load configuration: %e", err)
+				os.Exit(1)
 			}
-			StartServer(&cfg)
+			StartServer(cfg)
 		},
 	}
+	rootCmd.Flags().StringVarP(&config, "config", "c", "config.json", "path to the configuration file")
 
 	return rootCmd
 }
